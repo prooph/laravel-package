cff4a554f9d317a9438e227cc5cdd967d3e49c2f
Dmitriy Lezhnev <meekman74@gmail.com>
2017-06-16 20:00:33 +0500

    add Laravel queue message producer

---
+++ src/Job/HandleMessageJob.php
@@ -0,0 +1,118 @@
+<?php
+/**
+ * prooph (http://getprooph.org/)
+ *
+ * @see       https://github.com/prooph/laravel-package for the canonical source repository
+ * @copyright Copyright (c) 2016 prooph software GmbH (http://prooph-software.com/)
+ * @license   https://github.com/prooph/laravel-package/blob/master/LICENSE.md New BSD License
+ */
+
+declare(strict_types=1);
+
+namespace Prooph\Package\Job;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Prooph\Common\Messaging\DomainMessage;
+use Prooph\Common\Messaging\Message;
+use Prooph\Common\Messaging\MessageDataAssertion;
+
+class AsyncMessageHandler implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable;
+    
+    // Queue name (we can change priority for this queue by the name)
+    public $queue = 'prooph-queue';
+    
+    /** @var DomainMessage|string */
+    private $message;
+    
+    /**
+     * AsyncMessageHandler constructor.
+     *
+     * @param DomainMessage $message
+     */
+    public function __construct(DomainMessage $message)
+    {
+        $this->message = $message;
+    }
+    
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        // pass the message to the Message Bus
+        
+    }
+    
+    
+    /**
+     * Serialize Prooph message in order to persist it in the queue with no data corruption
+     *
+     *
+     * @param Message $message
+     *
+     * @return string
+     */
+    public function serializeMessage(DomainMessage $message): string
+    {
+        $message_data = $message->toArray();
+        MessageDataAssertion::assert($message_data);
+        
+        // replace DateTimeImmutable object with string representation
+        $message_data['created_at'] = $message->createdAt()->format('Y-m-d\TH:i:s.u');
+        
+        return json_encode($message_data);
+    }
+    
+    
+    /**
+     * Unserialize string and convert it to original Prooph message
+     *
+     *
+     * @param string $serialized_message
+     *
+     * @return DomainMessage
+     */
+    private function unserializeMessage(string $serialized_message): DomainMessage
+    {
+        $message_data = json_decode($serialized_message, true);
+        
+        $message_data['created_at'] = \DateTimeImmutable::createFromFormat(
+            'Y-m-d\TH:i:s.u',
+            $message_data['created_at'],
+            new \DateTimeZone('UTC')
+        );
+        
+        return DomainMessage::fromArray($message_data);
+    }
+    
+    /**
+     * __sleep called when object goes to the queue
+     * Ref: Illuminate/Queue/Queue@createObjectPayload()
+     *
+     *
+     * @return void
+     */
+    function __sleep()
+    {
+        $this->message = $this->serializeMessage($this->message);
+    }
+    
+    /**
+     * __wakeup called when job being pulled from the queue
+     *
+     * @return void
+     */
+    function __wakeup()
+    {
+        $this->message = $this->unserializeMessage($this->message);
+    }
+    
+    
+}
---
+++ src/Job/LaravelQueueMessageProducer.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * prooph (http://getprooph.org/)
+ *
+ * @see       https://github.com/prooph/laravel-package for the canonical source repository
+ * @copyright Copyright (c) 2016 prooph software GmbH (http://prooph-software.com/)
+ * @license   https://github.com/prooph/laravel-package/blob/master/LICENSE.md New BSD License
+ */
+
+declare(strict_types=1);
+
+namespace Prooph\Package\Job;
+
+use Prooph\Common\Messaging\Message;
+use Prooph\ServiceBus\Async\MessageProducer;
+use Prooph\ServiceBus\Exception\RuntimeException;
+use React\Promise\Deferred;
+
+final class LaravelQueueMessageProducer implements MessageProducer
+{
+    
+    public function __invoke(Message $message, Deferred $deferred = null): void
+    {
+        
+        // As far as I know - Laravel won't let me to get a promise, so no deferred queries are allowed
+        if ($deferred) {
+            throw new RuntimeException(__CLASS__ . ' cannot handle query messages which require future responses.');
+        }
+        
+        // Now dispatch the Laravel job to deal with the queue
+        dispatch(new HandleMessageJob($message));
+    }
+    
+}
