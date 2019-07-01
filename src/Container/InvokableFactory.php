eff40be26defacf429d3f17da2c37f05f91a9963
codeliner <kontakt@codeliner.ws>
2019-07-01 19:08:19 +0200

    Change copyright

--- src/Container/InvokableFactory.php
+++ src/Container/InvokableFactory.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
  * prooph (http://getprooph.org/)
  *
  * @see       https://github.com/prooph/laravel-package for the canonical source repository
- * @copyright Copyright (c) 2016 prooph software GmbH (http://prooph-software.com/)
+ * @copyright Copyright (c) 2016-2019 Alexander Miertsch <kontakt@codeliner.ws>
  * @license   https://github.com/prooph/laravel-package/blob/master/LICENSE.md New BSD License
  */
 
